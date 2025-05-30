4773e68a46eff00d024f546307c5f356e40e00ac
Majito Brinkitos <mcvef2013@gmail.com>
2025-05-30 10:24:18 -0700

    Lab Day 4 Maria Jose Mejia

--- lab-4-crud-zustand/src/App.tsx
+++ lab-4-crud-zustand/src/App.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import BlogList from './pages/BlogList';
+/* import BlogList from './pages/BlogList';
 import BlogDetail from './pages/BlogDetail';
 import AddPost from './pages/AddPost';
-import EditPost from './pages/EditPost';
+import EditPost from './pages/EditPost'; */
 import './App.css'
 
 function App() {
@@ -13,10 +13,10 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<BlogList />} />
+{/*         <Route path="/blog" element={<BlogList />} />
         <Route path="/blog:id" element={<BlogDetail />} />
         <Route path="/blog/new" element={<AddPost />} />
-        <Route path="/blog/edit/:id" element={<EditPost />} />
+        <Route path="/blog/edit/:id" element={<EditPost />} /> */}
       </Routes>
     </>
   )
--- lab-4-crud-zustand/src/pages/Home.tsx
+++ lab-4-crud-zustand/src/pages/Home.tsx
@@ -1,6 +1,14 @@
-function Home(){
-    return(
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+function Home(){
+
+    const Navigate = useNavigate();
+
+    return(
+        <div className=" grid grid-cols-3 gap-4 h-180 place-content-center bg-white">
+            <h1 className="col-start-2 col-end-3 border border-black rounded-md p-8 flex justify-center align-content-center bg-pink-900 text-white">Welcome to my Blog Page</h1>
+        </div>
     );
 }
 
